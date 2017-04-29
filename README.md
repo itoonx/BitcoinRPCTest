467e8586d6827b6a4176dad7f21992668a7c107e
theg0d <=pah-bravo@windowslive.com>
2017-04-29 18:59:58 +0700

    init

---
+++ README.md
@@ -0,0 +1,1 @@
+Test Bitcoind RPC Connection
---
+++ index.js
@@ -0,0 +1,15 @@
+const bitcoin = require('bitcoin');
+
+// all config options are optional
+const client = new bitcoin.Client({
+  host: '162.168.1.6',
+  port: 8332,
+  user: 'itoonx',
+  pass: 'BLOCKCHAIN_ONLY_WE_TRUTH',
+  timeout: 30000
+});
+
+client.getBalance('*', 6, function(err, balance, resHeaders) {
+  if (err) return console.log(err);
+  console.log('Balance:', balance);
+});
